57bd7cc8d40518e8278a72229b4a89ab217ac9e7
Balakumar3018 <balakumarjkp@gmail.com>
2022-01-12 11:41:55 +0530

    refactor- adding arrow functions

--- app.js
+++ app.js
@@ -2,32 +2,16 @@ let inputText=document.querySelector("#input-text-area");
 let btnTranslate=document.querySelector("#button-to-translate");
 let outputText=document.querySelector("#output-text-area");
 let serviceUrl="https://api.funtranslations.com/translate/navi.json";
-
-
+const ClickHandler=()=>{
+    let userText=inputText.value;
+    fetch(getTranslatorUrl(userText))
+    .then(response =>response.json())
+    .then(json => {
+    let translatedText=json.contents.translated;
+    outputText.innerText=translatedText;
+    })
+    .catch(errorHandler);
+    }
 btnTranslate.addEventListener("click", ClickHandler,false);
-
-
-
-//function url process
-function getTranslatorUrl(input){
-return serviceUrl+"?text="+input ;
-}
-
-//function for error handling 
-function errorHandler(error){
-console.log("Error occured ",error);
-alert("something wrong with server ! try again with some time");
-}        
-//funtion handler
-function ClickHandler(){
-let userText=inputText.value;
-
-//calling server api url
-fetch(getTranslatorUrl(userText))
-.then(response =>response.json())
-.then(json => {
-let translatedText=json.contents.translated;
-outputText.innerText=translatedText;
-})
-.catch(errorHandler);
-}
+const getTranslatorUrl=input=> serviceUrl+"?text="+input ;
+const errorHandler=error=> alert("something wrong with server ! try again with some time");
